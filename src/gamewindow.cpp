4f54013699cdc32b776f4866180e95cb4c575d61
Scott MacDonald <illynova@gmail.com>
2011-07-26 21:00:59 -0500

    adding some menus

--- src/gamewindow.cpp
+++ src/gamewindow.cpp
@@ -8,14 +8,65 @@
 GameWindow::GameWindow()
     : QMainWindow(),
       m_view( NULL ),
-      m_chat( new ChatWidget(this) )
+      m_chat( new ChatWidget(this) ),
+      m_fileMenu( NULL ),
+      m_helpMenu( NULL ),
+      m_connectAction( NULL ),
+      m_quitAction( NULL ),
+      m_aboutAction( NULL )
 {
     build();
     statusBar()->showMessage(tr("Hi there"));
+
+    configureActions();
+    configureMenus();
 }
 
 GameWindow::~GameWindow()
 {
+}
+
+void GameWindow::showlogin()
+{
+    qDebug() << "connect";
+}
+
+void GameWindow::close()
+{
+    qDebug() << "quit";
+}
+
+void GameWindow::about()
+{
+    qDebug() << "about";
+}
+
+void GameWindow::configureActions()
+{
+    m_connectAction = new QAction( tr("&Connect to server..."), this );
+    m_connectAction->setShortcuts( QKeySequence::Open );
+    m_connectAction->setStatusTip( tr("Connects to a forge server") );
+    connect( m_connectAction, SIGNAL(triggered()), this, SLOT(showlogin()) );
+
+    m_quitAction = new QAction( tr("&Quit"), this );
+    m_quitAction->setShortcuts( QKeySequence::Quit );
+    m_quitAction->setStatusTip( tr("Quits the game") );
+    connect( m_quitAction, SIGNAL(triggered()), this, SLOT(close()) );
+
+    m_aboutAction = new QAction( tr("&About"), this );
+    m_aboutAction->setStatusTip( tr("Shows information about this forge client") );
+    connect( m_aboutAction, SIGNAL(triggered()), this, SLOT(about()) );
+}
+
+void GameWindow::configureMenus()
+{
+    m_fileMenu = menuBar()->addMenu( tr("&File") );
+    m_fileMenu->addAction( m_connectAction );
+    m_fileMenu->addSeparator();
+    m_fileMenu->addAction( m_quitAction );
+
+    m_helpMenu = menuBar()->addMenu( tr("&Help") );
+    m_helpMenu->addAction( m_aboutAction );
 }
 
 void GameWindow::build()
--- src/gamewindow.h
+++ src/gamewindow.h
@@ -15,12 +15,24 @@ public:
     GameWindow();
     virtual ~GameWindow();
 
-private slots:
+public slots:
+    void showlogin();
+    void close();
+    void about();
+
+private:
     void build();
+    void configureActions();
+    void configureMenus();
 
 private:
     ClientView *m_view;
     ChatWidget *m_chat;
+    QMenu      *m_fileMenu;
+    QMenu      *m_helpMenu;
+    QAction    *m_connectAction;
+    QAction    *m_quitAction;
+    QAction    *m_aboutAction;
 };
 
 #endif
